e5d4eff555ac9ef6d230984229ebd275df700838
Rebeca Gaia <reb.gaia@gmail.com>
2021-09-16 22:38:12 -0300

    adicionando layout tela inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public options: Array<any> = [
+    { icon: 'person-add-outline', text: "Indicar amigos"},
+    { icon: 'phone-portrait-outline', text: "Recarga de celular"},
+    { icon: 'wallet-outline', text: "Depositar"},
+    { icon: 'options-outline', text: "Ajustar limite"},
+    { icon: 'help-circle-outline', text: "Me ajuda"},
+    { icon: 'barcode-outline', text: "Pagar"},
+    { icon: 'lock-open-outline', text: "Bloquear cartão"},
+    { icon: 'card-outline', text: "Cartão virtual"},
+  ];
 
   constructor() {}
 
